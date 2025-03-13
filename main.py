bafb63b4d174d4bb35c5a2d9214ae4247df1047e
Aergolis <adamufilip01@gmail.com>
2025-03-13 17:59:30 +0100

    Přidán .gitignore s ignorování textu knih a main.py pro otevření knihy.

---
+++ main.py
@@ -0,0 +1,10 @@
+def main():
+    book_text = get_book_text("books/frankenstein.txt")
+    print (book_text)
+
+def get_book_text(filepath):
+    with open(filepath) as f:
+        file_contents = f.read()
+    return file_contents
+
+main()
