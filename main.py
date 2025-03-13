ee3f8c2c4923419989e56a55996947aaa0e07bb9
Aergolis <adamufilip01@gmail.com>
2025-03-13 18:40:31 +0100

    Přidán stats.py pro trackování a upraven main pro importování stats.py

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+from stats import get_num_words
+
 def main():
     book_text = get_book_text("books/frankenstein.txt")
     print (book_text)
+    total_words = get_num_words(book_text)
+    print (f"{total_words} words found in the document")
 
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
     return file_contents
+
 
 main()
---
+++ stats.py
@@ -0,0 +1,4 @@
+def get_num_words(num_word):
+    total_words = len(num_word.split())
+    return total_words
+    
