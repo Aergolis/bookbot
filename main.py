f15e8a6f5ead6a610b01edc6b5f0bc9cea1a63ee
Aergolis <adamufilip01@gmail.com>
2025-03-13 19:15:37 +0100

    Přidána funkce na počítání písmen a implementována do main.py

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from stats import get_num_words
+from stats import get_character_count
 
 def main():
     book_text = get_book_text("books/frankenstein.txt")
-    print (book_text)
     total_words = get_num_words(book_text)
     print (f"{total_words} words found in the document")
+    char_count = get_character_count(book_text)
+    for char, count in char_count.items():
+        print(f"'{char}': {count}")
+    
 
 def get_book_text(filepath):
     with open(filepath) as f:
--- stats.py
+++ stats.py
@@ -1,4 +1,15 @@
+
+
 def get_num_words(num_word):
     total_words = len(num_word.split())
     return total_words
     
+def get_character_count(text):
+    char_dic = {}
+    for char in text:
+        l_char = char.lower()
+        if l_char in char_dic:
+            char_dic[l_char] += 1
+        else:
+            char_dic[l_char] = 1
+    return char_dic
