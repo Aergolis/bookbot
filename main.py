478abe1ddaca25c37f076e406ce7cd0a5c4b8ddd
Aergolis <adamufilip01@gmail.com>
2025-03-14 11:37:11 +0100

    Přetvoření main.py pro lepší čitelnost - přidána funkce sys pro otevírání souboru a získání filepath -Přidány popisky -Importace z stats.py
    Vytvoření třídícího systému dle výskytu a importováno do main.py
    -přidány popisky

--- main.py
+++ main.py
@@ -1,18 +1,42 @@
-from stats import get_num_words
-from stats import get_character_count
+from stats import get_num_words         # Importování funkcí z jiného souboru
+from stats import get_character_count   # Importování funkcí z jiného souboru
+from stats import sort_char_by_count    # Importování funkcí z jiného souboru
+import sys                              # Importování knihovny sys
+
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
-    total_words = get_num_words(book_text)
-    print (f"{total_words} words found in the document")
-    char_count = get_character_count(book_text)
-    for char, count in char_count.items():
-        print(f"'{char}': {count}")
-    
+    # filepath = input("Enter the path to the book you want to analyze: ") Jiné řešení
 
-def get_book_text(filepath):
-    with open(filepath) as f:
-        file_contents = f.read()
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")  # Pokud není zadán argument, vypíše se nápověda
+        sys.exit(1)
+    else:
+        filepath = sys.argv[1]      # Získání cesty k souboru z argumentů
+
+    book_text = get_book_text(filepath)     # Získání textu z knihy
+    total_words = get_num_words(book_text)  # Získání počtu slov
+
+
+    char_count = get_character_count(book_text)     # Získání počtu znaků
+    sorted_chars = sort_char_by_count(char_count)   # Seřazení znaků podle počtu výskytů
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}...")
+    print("----------- Word Count ----------")
+    print(f"Found {total_words} total words")
+    print("--------- Character Count -------")
+    
+    for char_info in sorted_chars:  # Výpis znaků a jejich počtu
+        char = char_info["char"]        # Získání znaku
+        count = char_info["count"]      # Získání počtu výskytů
+        if char.isalpha():          # Pokud je znak písmeno, vypíše se
+            print(f"{char}: {count}")
+
+    print("============= END ===============")
+
+def get_book_text(filepath):         # Funkce pro získání textu z knihy
+    with open(filepath) as f:        # Otevření souboru
+        file_contents = f.read()     # Přečtení obsahu souboru
     return file_contents
 
 
--- stats.py
+++ stats.py
@@ -1,15 +1,26 @@
 
 
-def get_num_words(num_word):
-    total_words = len(num_word.split())
+def get_num_words(num_word):                # Funkce pro získání počtu slov
+    total_words = len(num_word.split())     # Rozdělení textu podle mezer a získání délky
     return total_words
     
-def get_character_count(text):
-    char_dic = {}
-    for char in text:
-        l_char = char.lower()
-        if l_char in char_dic:
+def get_character_count(text):      # Funkce pro získání počtu znaků
+    char_dic = {}               # Vytvoření slovníku pro znaky
+    for char in text:           # Procházení znaků v textu
+        l_char = char.lower()   # Převedení do malých písmen
+        if l_char in char_dic:      # Pokud znak již existuje, přičte se 1
             char_dic[l_char] += 1
         else:
-            char_dic[l_char] = 1
+            char_dic[l_char] = 1    # Jinak se vytvoří nový záznam
     return char_dic
+
+def sort_on(dict_item):        # Funkce pro seřazení znaků podle počtu výskytů
+    return dict_item["count"]       # Vrátí počet výskytů
+
+def sort_char_by_count(char_count): # Funkce pro seřazení znaků podle počtu výskytů
+    char_list = []                  # Vytvoření listu pro znaky
+    for char, count in char_count.items():                  # Procházení znaků a jejich počtu
+        char_list.append({"char": char, "count": count})    # Přidání znaku a jeho počtu do listu
+    char_list.sort(key=sort_on, reverse=True)               # Seřazení listu podle počtu výskytů
+    return char_list
+
